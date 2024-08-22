9755ab83069600b2c63a99c5f78613adea3d312f
Omkar Kirpan <okirpan@gmail.com>
2024-08-22 19:53:33 +0530

    feat: update backend  add stats route to expose analytics data

--- server/index.js
+++ server/index.js
@@ -9,6 +9,7 @@ app.use(express.json());
 import Chance from "chance";
 
 const chance = new Chance();
+const searchFrequency = {};
 
 const animals = [...Array(250).keys()].map((id) => {
   return {
@@ -23,12 +24,41 @@ const animals = [...Array(250).keys()].map((id) => {
 app.get("", (req, res) => {
   // Filter results by query
   const q = req.query.q?.toLowerCase() || "";
+
+  if (q) {
+    searchFrequency[q] = (searchFrequency[q] || 0) + 1;
+  }
+
   const results = animals.filter((animal) =>
     animal.type.toLowerCase().includes(q),
   );
 
   res.send(results);
 });
+
+app.get("/stats", (req, res) => {
+  const totalAnimals = animals.length;
+
+  const animalsByTypes = animals.reduce((acc, animal) => {
+    acc[animal.type] = (acc[animal.type] || 0) + 1;
+    return acc;
+  }, {});
+
+  const totalAnimalTypes = Object.keys(animalsByTypes).length;
+
+  const averageAge = (
+    animals.reduce((acc, animal) => acc + animal.age, 0) / totalAnimals
+  ).toFixed(2);
+
+  res.send({
+    totalAnimals,
+    totalAnimalTypes,
+    averageAge,
+    searchFrequency,
+    animalsByTypes,
+  });
+});
+
 const PORT = 8080;
 app.listen(PORT, () =>
   console.log(`⚡️ Listening on port http://localhost:${PORT}`),
