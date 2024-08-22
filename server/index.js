95516f8185851427f8607f1230ef9361ef6e3d10
Omkar Kirpan <okirpan@gmail.com>
2024-08-22 16:27:01 +0530

    feat: add server  generate animal farm data

---
+++ server/index.js
@@ -0,0 +1,35 @@
+import express from "express";
+import cors from "cors";
+// initialize the express app
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+// make some animals
+import Chance from "chance";
+
+const chance = new Chance();
+
+const animals = [...Array(250).keys()].map((id) => {
+  return {
+    id,
+    type: chance.animal(),
+    age: chance.age(),
+    name: chance.name(),
+  };
+});
+
+// Endpoint to search for animals
+app.get("", (req, res) => {
+  // Filter results by query
+  const q = req.query.q?.toLowerCase() || "";
+  const results = animals.filter((animal) =>
+    animal.type.toLowerCase().includes(q),
+  );
+
+  res.send(results);
+});
+const PORT = 8080;
+app.listen(PORT, () =>
+  console.log(`⚡️ Listening on port http://localhost:${PORT}`),
+);
