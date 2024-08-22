bd2b2b7d71637c733eb55a19dd9aba74be759a92
Omkar Kirpan <okirpan@gmail.com>
2024-08-22 18:30:19 +0530

    feat: update frontend cache search query in localstorage

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,15 @@
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import "./App.css";
 
 function App() {
   const [animals, setAnimals] = useState([]);
+  const inputRef = useRef(null);
+
+  useEffect(() => {
+    const lastQuery = localStorage.getItem("lastQuery");
+    search(lastQuery);
+    inputRef.current.value = lastQuery;
+  }, []);
 
   const search = async (q) => {
     const response = await fetch(
@@ -10,12 +17,15 @@ function App() {
     );
     const data = await response.json();
     setAnimals(data);
+
+    localStorage.setItem("lastQuery", q);
   };
 
   return (
     <>
       <h1>Animal Farm</h1>
       <input
+        ref={inputRef}
         type="text"
         placeholder="🔍 Search..."
         onChange={(e) => search(e.target.value)}
@@ -23,10 +33,6 @@ function App() {
       <div>
         <ul>
           {animals.map((animal) => (
-            // <li key={animal.id}>
-            //   <strong>{animal.type}</strong>
-            //   {animal.name} {animal.age}
-            // </li>;
             <Animal key={animal.id} {...animal} />
           ))}
           {animals.length === 0 && "No animals found."}
